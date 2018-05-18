35e156c02852934d8a25f9d8fe8f74d2237653c7
Xhendos <youri_business@outlook.com>
2018-05-18 21:11:23 +0000

    Finished gpio::export, gpio::set_direction and gpio::set_value

--- src/Gpio/main.cpp
+++ src/Gpio/main.cpp
@@ -7,16 +7,25 @@
 
 int main()
 {
-    gpio::gpio_header h;
-    h.pin = 11;
-    h.direction = DIR_IN;
-    h.value = 1;
-    h.edge = EDGE_NONE;
-    h.active_low = 0;
+    gpio::export_gpio(7);
+	gpio::export_gpio(8);
 
-    gpio::export_gpio(h);
+	sleep(2);
 
-    sleep(15);
+	gpio::set_direction(7, DIR_OUT);
+	gpio::set_direction(8, DIR_OUT);
+
+	sleep(2);
+
+	gpio::set_value(8, 1);
+
+	while(1)
+	{
+		gpio::set_value(7, 1);
+		usleep(250);
+		gpio::set_value(7, 0);
+		usleep(250);
+	}
 
     return 0;
 }
---
+++ src/Gpio/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo 7 > /sys/class/gpio/export
+echo 8 > /sys/class/gpio/export
+
+echo "out" > /sys/class/gpio/gpio7/direction
+echo "out" > /sys/class/gpio/gpio8/direction
+
+echo 1 > /sys/class/gpio/gpio8/value
+
+while true
+do
+	echo 1 > /sys/class/gpio/gpio7/value
+	#sleep 0.05
+	echo 0 > /sys/class/gpio/gpio7/value
+	#sleep 0.05
+done
+
+
